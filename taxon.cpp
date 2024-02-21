1a3dec1fbe1c20f171ce8cad28df87c23b338a13
LIU Hao <lh_mouse@126.com>
2024-02-22 00:54:06 +0800

    taxon: Implement `print()`

--- taxon.cpp
+++ taxon.cpp
@@ -4,10 +4,11 @@
 #include "taxon.hpp"
 #include <rocket/tinybuf_str.hpp>
 #include <rocket/tinybuf_file.hpp>
+#include <cmath>
+#include <cstdio>
+#include <clocale>
+#include <cuchar>
 namespace taxon {
-namespace {
-
-}  // namespace
 
 Value::
 ~Value()
@@ -43,6 +44,25 @@ Value::
     }
   }
 
+static
+void
+do_check_global_locale() noexcept
+  {
+    static constexpr char keywords[][8] = { ".utf8", ".utf-8", ".UTF-8" };
+    const char* locale = ::std::setlocale(LC_ALL, nullptr);
+    const char* found = nullptr;
+
+    if(locale)
+      for(const char* kw : keywords)
+        if((found = ::std::strstr(locale, kw)) != nullptr)
+          break;
+
+    if(!found)
+      ::std::fprintf(stderr,
+            "WARNING: Please set a UTF-8 locale instead of `%s`.\n",
+            locale ? locale : "(no locale)");
+  }
+
 bool
 Value::
 parse(::rocket::tinybuf& buf, Parser_Result* result_opt)
@@ -70,11 +90,338 @@ parse_file(::FILE* fp, Parser_Result* result_opt)
     return succ;
   }
 
+static
+void
+do_print_utf8_string(::rocket::tinybuf& buf, ::rocket::ascii_numput& nump,
+                     const ::rocket::cow_string& str)
+  {
+    const char* bptr = str.c_str();
+    const char* const eptr = str.c_str() + str.length();
+    ::std::mbstate_t mbstate = { };
+    char16_t c16;
+    ::std::size_t cr;
+
+    while(bptr != eptr) {
+      cr = ::std::mbrtoc16(&c16, bptr, static_cast<::std::size_t>(eptr - bptr), &mbstate);
+      switch(static_cast<int>(cr))
+        {
+        case -3:
+          // No input has been consumed. A trailing surrogate has been stored into
+          // `c16`. Print `c16`.
+          break;
+
+        case -2:
+          // The input is complete but has been consumed entirely. As we have passed
+          // the entire string, this means it is invalid. Consume all bytes but
+          // print a replacement character.
+          bptr = eptr;
+          c16 = u'\uFFFD';
+          break;
+
+        case -1:
+          // An invalid byte has been encountered. Consume one byte but print a
+          // replacement character. The `mbstate_t` structure shall be reset for the
+          // next character.
+          mbstate = { };
+          bptr ++;
+          c16 = u'\uFFFD';
+          break;
+
+        case 0:
+          // A null byte has been consumed and stored into `c16`.
+          bptr ++;
+          break;
+
+        default:
+          // `cr` bytes have been consumed, converted and stored into `c16`.
+          bptr += cr;
+          break;
+        }
+
+      if(c16 == u'\"') {
+        // `\"`
+        buf.putn("\\\"", 2);
+      }
+      else if(c16 == u'\\') {
+        // `\\`
+        buf.putn("\\\\", 2);
+      }
+      else if(c16 == u'\b') {
+        // `\b`; backspace
+        buf.putn("\\b", 2);
+      }
+      else if(c16 == u'\f') {
+        // `\f`; form feed
+        buf.putn("\\f", 2);
+      }
+      else if(c16 == u'\n') {
+        // `\n`; line feed
+        buf.putn("\\n", 2);
+      }
+      else if(c16 == u'\r') {
+        // `\r`; carrige return
+        buf.putn("\\r", 2);
+      }
+      else if(c16 == u'\t') {
+        // `\t`; horizontal tab
+        buf.putn("\\t", 2);
+      }
+      else if((c16 >= 0x20) && (c16 <= 0x7E)) {
+        // ASCII printable
+        buf.putc(static_cast<char>(c16));
+      }
+      else {
+        // UTF-16
+        nump.put_XU(c16, 4);
+        char temp[] = { '\\', 'u', nump[2], nump[3], nump[4], nump[5] };
+        buf.putn(temp, sizeof(temp));
+      }
+    }
+  }
+
+static
+void
+do_print_binary_in_hex(::rocket::tinybuf& buf, ::rocket::ascii_numput& nump,
+                       const ::rocket::cow_bstring& bin)
+  {
+    const unsigned char* bptr = bin.data();
+    const unsigned char* const eptr = bin.data() + bin.size();
+    ::std::uint64_t word;
+
+    while(eptr - bptr >= 8) {
+      // 8-byte group
+      ::std::memcpy(&word, bptr, 8);
+      bptr += 8;
+      nump.put_XU(ROCKET_BETOH64(word), 16);
+      buf.putn(nump.data() + 2, 16);
+    }
+
+    if(eptr - bptr >= 4) {
+      // 4-byte group
+      ::std::memcpy(&word, bptr, 4);
+      bptr += 4;
+      nump.put_XU(ROCKET_BETOH64(word), 16);
+      buf.putn(nump.data() + 2, 8);
+    }
+
+    while(bptr != eptr) {
+      // bytewise
+      nump.put_XU(*bptr, 2);
+      bptr ++;
+      buf.putn(nump.data() + 2, 2);
+    }
+  }
+
+static constexpr
+char
+do_get_base64_digit(::std::uint32_t word) noexcept
+  {
+    return (word < 26) ? static_cast<char>(u'A' + word)
+         : (word < 52) ? static_cast<char>(u'a' + word - 26)
+         : (word < 62) ? static_cast<char>(u'0' + word - 52)
+         : (word < 63) ? '+' : '/';
+  }
+
+static
+void
+do_print_binary_in_base64(::rocket::tinybuf& buf, const ::rocket::cow_bstring& bin)
+  {
+    const unsigned char* bptr = bin.data();
+    const unsigned char* const eptr = bin.data() + bin.size();
+    ::std::ptrdiff_t remainder;
+    ::std::uint32_t word;
+    char b64word[4];
+
+    while(eptr - bptr >= 3) {
+      // 3-byte group
+      ::std::memcpy(&word, bptr, 4);  // make use of the null terminator!
+      bptr += 3;
+      word = ROCKET_BETOH32(word);
+
+      b64word[0] = do_get_base64_digit(word >> 26);
+      b64word[1] = do_get_base64_digit(word >> 20 & 0x3F);
+      b64word[2] = do_get_base64_digit(word >> 14 & 0x3F);
+      b64word[3] = do_get_base64_digit(word >>  8 & 0x3F);
+      buf.putn(b64word, 4);
+    }
+
+    if(bptr != eptr) {
+      // 1-byte or 2-byte group
+      remainder = eptr - bptr;
+      word = 0;
+      ::std::memcpy(&word, bptr, 2);  // make use of the null terminator!
+      bptr = eptr;
+      word = ROCKET_BETOH32(word);
+
+      b64word[0] = do_get_base64_digit(word >> 26);
+      b64word[1] = do_get_base64_digit(word >> 20 & 0x3F);
+      b64word[2] = (remainder == 1) ? '=' : do_get_base64_digit(word >> 14 & 0x3F);
+      b64word[3] = '=';
+      buf.putn(b64word, 4);
+    }
+  }
+
 ::rocket::tinybuf&
 Value::
 print(::rocket::tinybuf& buf) const
   {
-// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
+    do_check_global_locale();
+
+    // Break deep recursion with a handwritten stack.
+    struct xFrame
+      {
+        char closure;
+        const V_array* psa;
+        V_array::const_iterator ita;
+        const V_object* pso;
+        V_object::const_iterator ito;
+      };
+
+    ::rocket::cow_vector<xFrame> stack;
+    ::rocket::ascii_numput nump;
+    static constexpr ::std::uint8_t use_hex_for[] = { 1, 2, 4, 8, 16, 20, 32 };
+    const Variant* pstor = &(this->m_stor);
+
+  do_unpack_loop_:
+    switch(static_cast<Type>(pstor->index()))
+      {
+      case t_null:
+        buf.putn("null", 4);
+        break;
+
+      case t_array:
+        if(!pstor->as<V_array>().empty()) {
+          // open
+          auto& frm = stack.emplace_back();
+          frm.closure = ']';
+          frm.psa = &(pstor->as<V_array>());
+          frm.ita = frm.psa->begin();
+          buf.putc('[');
+          pstor = &(frm.ita->m_stor);
+          goto do_unpack_loop_;
+        }
+        buf.putn("[]", 2);
+        break;
+
+      case t_object:
+        if(!pstor->as<V_object>().empty()) {
+          // open
+          auto& frm = stack.emplace_back();
+          frm.closure = '}';
+          frm.pso = &(pstor->as<V_object>());
+          frm.ito = frm.pso->begin();
+          buf.putn("{\"", 2);
+          do_print_utf8_string(buf, nump, frm.ito->first.rdstr());
+          buf.putn("\":", 2);
+          pstor = &(frm.ito->second.m_stor);
+          goto do_unpack_loop_;
+        }
+        buf.putn("{}", 2);
+        break;
+
+      case t_boolean:
+        nump.put_TB(pstor->as<V_boolean>());
+        buf.putn(nump.data(), nump.size());
+        break;
+
+      case t_integer:
+        nump.put_DI(pstor->as<V_integer>());
+        buf.putn("\"$l:", 4);
+        buf.putn(nump.data(), nump.size());
+        buf.putc('\"');
+        break;
+
+      case t_number:
+        if(::std::isfinite(pstor->as<V_number>())) {
+          // finite; unquoted
+          nump.put_DD(pstor->as<V_number>());
+          buf.putn(nump.data(), nump.size());
+        }
+        else {
+          // non-finite; annotated
+          buf.putn("\"$d:", 4);
+          nump.put_DD(pstor->as<V_number>());
+          buf.putn(nump.data(), nump.size());
+          buf.putc('\"');
+        }
+        break;
+
+      case t_string:
+        if(pstor->as<V_string>()[0] != '$') {
+          // general; quoted
+          buf.putc('\"');
+          do_print_utf8_string(buf, nump, pstor->as<V_string>());
+          buf.putc('\"');
+        }
+        else {
+          // starts with `$`; annotated
+          buf.putn("\"$s:", 4);
+          do_print_utf8_string(buf, nump, pstor->as<V_string>());
+          buf.putc('\"');
+        }
+        break;
+
+      case t_binary:
+        if(::rocket::is_any_of(pstor->as<V_binary>().size(), use_hex_for)) {
+          // short; hex
+          buf.putn("\"$h:", 4);
+          do_print_binary_in_hex(buf, nump, pstor->as<V_binary>());
+          buf.putc('\"');
+        }
+        else {
+          // general; base64
+          buf.putn("\"$b:", 4);
+          do_print_binary_in_base64(buf, pstor->as<V_binary>());
+          buf.putc('\"');
+        }
+        break;
+
+      case t_time:
+        nump.put_DI(::std::chrono::time_point_cast<::std::chrono::milliseconds>(
+                                 pstor->as<V_time>()).time_since_epoch().count());
+        buf.putn("\"$t:", 4);
+        buf.putn(nump.data(), nump.size());
+        buf.putc('\"');
+        break;
+
+      default:
+        ::rocket::sprintf_and_throw<::std::invalid_argument>(
+              "taxon::Value: unknown type enumeration `%d`",
+              static_cast<int>(pstor->index()));
+      }
+
+    while(!stack.empty()) {
+      auto& frm = stack.mut_back();
+      switch(frm.closure)
+        {
+        case ']':
+          if(++ frm.ita != frm.psa->end()) {
+            // next
+            buf.putc(',');
+            pstor = &(frm.ita->m_stor);
+            goto do_unpack_loop_;
+          }
+          break;
+
+        case '}':
+          if(++ frm.ito != frm.pso->end()) {
+            // next
+            buf.putn(",\"", 2);
+            do_print_utf8_string(buf, nump, frm.ito->first.rdstr());
+            buf.putn("\":", 2);
+            pstor = &(frm.ito->second.m_stor);
+            goto do_unpack_loop_;
+          }
+          break;
+
+        default:
+          ROCKET_UNREACHABLE();
+        }
+
+      // close
+      buf.putc(frm.closure);
+      stack.pop_back();
+    }
 
     return buf;
   }
@@ -102,7 +449,7 @@ print_to_file(::FILE* fp) const
   {
     ::rocket::tinybuf_file buf(fp, nullptr);
     this->print(buf);
-    return ::ferror(buf.get_handle()) == 0;
+    return ::std::ferror(buf.get_handle()) == 0;
   }
 
 }  // namespace taxon
--- test_main.cpp
+++ test_main.cpp
@@ -58,6 +58,7 @@ operator delete[](void* ptr, ::std::size_t) noexcept
 int
 main(void)
   {
+    ::setlocale(LC_ALL, "C.UTF-8");
     delete new int;
     ::alloc_count = 0;
 
@@ -65,12 +66,14 @@ main(void)
       ::taxon::Value val;
       assert(val.type() == ::taxon::t_null);
       assert(val.is_null());
+      assert(val.print_to_string() == "null");
     }
 
     {
       ::taxon::Value val = nullptr;
       assert(val.type() == ::taxon::t_null);
       assert(val.is_null());
+      assert(val.print_to_string() == "null");
     }
 
     {
@@ -85,18 +88,21 @@ main(void)
       assert(val.as_array().at(1).as_string() == "hello");
       assert(val.as_array().at(2).type() == ::taxon::t_boolean);
       assert(val.as_array().at(2).as_boolean() == false);
+      assert(val.print_to_string() == R"(["$l:1","hello",false])");
     }
 
     {
-      ::taxon::V_object obj = { { &"x", 1 }, { &"y", &"hello" } };
+      ::taxon::V_object obj = { { &"x", 3.5 }, { &"y", &"hello" } };
       ::taxon::Value val = obj;
       assert(val.type() == ::taxon::t_object);
       assert(val.is_object());
       assert(val.as_object().size() == 2);
-      assert(val.as_object().at(&"x").type() == ::taxon::t_integer);
-      assert(val.as_object().at(&"x").as_integer() == 1);
+      assert(val.as_object().at(&"x").type() == ::taxon::t_number);
+      assert(val.as_object().at(&"x").as_number() == 3.5);
       assert(val.as_object().at(&"y").type() == ::taxon::t_string);
       assert(val.as_object().at(&"y").as_string() == "hello");
+      assert(val.print_to_string() == R"({"x":3.5,"y":"hello"})"
+             || val.print_to_string() == R"({"y":"hello","x":3.5})");
     }
 
     {
@@ -104,6 +110,7 @@ main(void)
       assert(val.type() == ::taxon::t_boolean);
       assert(val.is_boolean());
       assert(val.as_boolean() == true);
+      assert(val.print_to_string() == "true");
 
       val.mut_integer() = 42;
       assert(val.type() == ::taxon::t_integer);
@@ -116,6 +123,7 @@ main(void)
       assert(val.type() == ::taxon::t_integer);
       assert(val.is_integer());
       assert(val.as_integer() == 0);
+      assert(val.print_to_string() == R"("$l:0")");
 
       val.mut_number() = -5;
       assert(val.is_number());
@@ -127,6 +135,7 @@ main(void)
       assert(val.type() == ::taxon::t_integer);
       assert(val.is_integer());
       assert(val.as_integer() == INT64_MAX);
+      assert(val.print_to_string() == R"("$l:9223372036854775807")");
     }
 
     {
@@ -134,6 +143,7 @@ main(void)
       assert(val.type() == ::taxon::t_integer);
       assert(val.is_integer());
       assert(val.as_integer() == INT64_MIN);
+      assert(val.print_to_string() == R"("$l:-9223372036854775808")");
     }
 
     {
@@ -141,6 +151,7 @@ main(void)
       assert(val.type() == ::taxon::t_number);
       assert(val.is_number());
       assert(val.as_number() == 1.5);
+      assert(val.print_to_string() == "1.5");
 
       val = &"world";
       assert(val.type() == ::taxon::t_string);
@@ -155,6 +166,7 @@ main(void)
       assert(val.type() == ::taxon::t_number);
       assert(val.is_number());
       assert(::std::isnan(val.as_number()));
+      assert(val.print_to_string() == R"("$d:nan")");
     }
 
     {
@@ -163,16 +175,18 @@ main(void)
       assert(val.is_number());
       assert(val.as_number() == 42.0);
       assert(val.type() == ::taxon::t_integer);
+      assert(val.print_to_string() == R"("$l:42")");
     }
 
     {
-      ::taxon::Value val = &"hello";
+      ::taxon::Value val = &"\b\f\n\r\t\x1B\x7F";
       assert(val.type() == ::taxon::t_string);
       assert(val.is_string());
-      assert(val.as_string().size() == 5);
-      assert(val.as_string() == "hello");
-      assert(val.as_string_length() == 5);
-      assert(::memcmp(val.as_string_c_str(), "hello", 6) == 0);
+      assert(val.as_string().size() == 7);
+      assert(val.as_string() == "\b\f\n\r\t\x1B\x7F");
+      assert(val.as_string_length() == 7);
+      assert(::memcmp(val.as_string_c_str(), "\b\f\n\r\t\x1B\x7F", 8) == 0);
+      assert(val.print_to_string() == R"("\b\f\n\r\t\u001B\u007F")");
 
       static constexpr unsigned char bytes[] = "\xFF\x00\xFE\x7F\x80";
       val = ::rocket::cow_bstring(bytes, 5);
@@ -181,17 +195,19 @@ main(void)
       assert(val.as_binary().compare(bytes, 5) == 0);
       assert(val.as_binary_size() == 5);
       assert(::memcmp(val.as_binary_data(), bytes, 5) == 0);
+      assert(val.print_to_string() == R"("$b:/wD+f4A=")");
     }
 
     {
-      static constexpr unsigned char bytes[] = "\x01\x00\x03";
-      ::taxon::Value val = ::rocket::cow_bstring(bytes, 3);
+      static constexpr unsigned char bytes[] = "\x01\x00\x03\xC0";
+      ::taxon::Value val = ::rocket::cow_bstring(bytes, 4);
       assert(val.type() == ::taxon::t_binary);
       assert(val.is_binary());
-      assert(val.as_binary().size() == 3);
-      assert(val.as_binary().compare(bytes, 3) == 0);
-      assert(val.as_binary_size() == 3);
-      assert(::memcmp(val.as_binary_data(), bytes, 3) == 0);
+      assert(val.as_binary().size() == 4);
+      assert(val.as_binary().compare(bytes, 4) == 0);
+      assert(val.as_binary_size() == 4);
+      assert(::memcmp(val.as_binary_data(), bytes, 4) == 0);
+      assert(val.print_to_string() == R"("$h:010003C0")");
 
       static constexpr ::std::chrono::milliseconds dur(123456789);
       static constexpr ::std::chrono::system_clock::time_point tp(dur);
@@ -208,6 +224,7 @@ main(void)
       assert(val.type() == ::taxon::t_time);
       assert(val.is_time());
       assert(val.as_time() == tp);
+      assert(val.print_to_string() == R"("$t:123456789")");
 
       val = false;
       assert(val.type() == ::taxon::t_boolean);
@@ -216,17 +233,16 @@ main(void)
     }
 
     {
-      // `["meow",{"x":true},12.5,[37,null]]`
+      // `["$s:meow",{"x":true},12.5,[37,null]]`
       ::taxon::Value val;
       val.mut_array().resize(4);
-      val.mut_array().mut(0) = &"meow";
+      val.mut_array().mut(0) = &"$meow";
       val.mut_array().mut(1).mut_object().try_emplace(&"x", true);
       val.mut_array().mut(2) = 12.5;
       val.mut_array().mut(3).mut_array().resize(2);
       val.mut_array().mut(3).mut_array().mut(0) = 37;
       val.mut_array().mut(3).mut_array().mut(1) = nullptr;
-
-      // TODO
+      assert(val.print_to_string() == R"(["$s:$meow",{"x":true},12.5,["$l:37",null]])");
     }
 
     // leak check
